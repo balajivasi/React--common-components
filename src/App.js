30a1ce57202668b82fe6f4306c7379b154799e38
Balaji Vasi <balajivasi@Balajis-MacBook-Pro.local>
2018-02-25 17:28:51 -0500

    react common components first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Alert from './components/Alert'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Alert />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Alert/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Alert extends React.Component{
+    render(){
+        return <h1>Alert component</h1>
+    }
+}
